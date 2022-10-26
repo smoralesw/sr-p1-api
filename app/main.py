81450741682d0c007c17ceea5dfe268c698fd8af
smoralesw <s.moralesw@uc.cl>
2022-10-26 14:10:20 -0300

    crud working

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-# import models
-# from routes import router
-# from config import engine
+import models
+from routes import router
+from config import engine
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
-async def Home():
-    return "Hello World"
-
-# app.include_router(router, prefix="/book", tags=["book"])
+app.include_router(router, prefix="")
